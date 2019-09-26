ffc4014860dbf98082bfffc74e4265984ae8d88d
Aleksandar Vasic <coavasic96@gmail.com>
2019-09-26 15:48:22 +0200

    Refactor KategorijaController

--- src/main/java/vasic/prodavnica/racunara/controller/KategorijaController.java
+++ src/main/java/vasic/prodavnica/racunara/controller/KategorijaController.java
@@ -5,13 +5,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import vasic.prodavnica.racunara.dto.KategorijaDTO;
-import vasic.prodavnica.racunara.exceptions.CategoryNotFoundException;
-import vasic.prodavnica.racunara.exceptions.InvalidRequestBodyException;
+import vasic.prodavnica.racunara.exceptions.EntityNotFoundException;
 import vasic.prodavnica.racunara.model.Kategorija;
 import vasic.prodavnica.racunara.service.KategorijaService;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 @RestController
@@ -24,34 +22,35 @@ public class KategorijaController {
     @GetMapping("/kategorije/all")
     public ResponseEntity<List<KategorijaDTO>> getAll() {
 
-        List<KategorijaDTO> dtos = kategorijaService.getAll().stream().map(KategorijaDTO::new).collect(Collectors.toList());
-
+        List<KategorijaDTO> dtos = kategorijaService.getAll().stream()
+                .map(KategorijaDTO::new)
+                .collect(Collectors.toList());
 
         return new ResponseEntity<List<KategorijaDTO>>(dtos, HttpStatus.OK);
     }
 
     @GetMapping("/kategorije/{id}")
     public ResponseEntity<KategorijaDTO> getById(@PathVariable Integer id) {
-        return new ResponseEntity<KategorijaDTO>(new KategorijaDTO(kategorijaService.getOneById(id).orElseThrow(() -> new CategoryNotFoundException("Category not found"))), HttpStatus.OK);
+
+        KategorijaDTO kategorijaDTO = kategorijaService.getOneById(id).map(KategorijaDTO::new).orElseThrow(
+                () -> new EntityNotFoundException(Kategorija.class.getName(), id)
+        );
+
+        return new ResponseEntity<KategorijaDTO>(kategorijaDTO, HttpStatus.OK);
     }
 
     @PostMapping("/kategorije")
-    public ResponseEntity<KategorijaDTO> save(@RequestBody Optional<KategorijaDTO> optionalKategorijaDTO) {
-        optionalKategorijaDTO.orElseThrow(InvalidRequestBodyException::new);
+    public ResponseEntity<KategorijaDTO> save(@RequestBody KategorijaDTO kategorijaDTO) {
 
         Kategorija kategorija = new Kategorija();
+        kategorija.setNaziv(kategorijaDTO.getNaziv());
+        kategorija.setOpis(kategorijaDTO.getOpis());
 
-        optionalKategorijaDTO.map(kategorijaDTO -> {
-            kategorija.setNaziv(kategorijaDTO.getNaziv());
-            kategorija.setOpis(kategorijaDTO.getOpis());
-            if (kategorijaDTO.getPodkategorijaId() != 0) {
-                kategorija.setPodkategorija(kategorijaService.getOneById(kategorijaDTO.getPodkategorijaId()).orElseThrow(() -> new CategoryNotFoundException("Category not found")));
-            } else {
-                kategorija.setPodkategorija(null);
-            }
+        kategorija.setPodkategorija(kategorijaDTO.getPodkategorijaId() < 1
+                ? null : kategorijaService.getOneById(kategorijaDTO.getPodkategorijaId()).orElseThrow(
+                () -> new EntityNotFoundException(Kategorija.class.getName(), kategorijaDTO.getPodkategorijaId())
+        ));
 
-            return null;
-        });
         return new ResponseEntity<KategorijaDTO>(new KategorijaDTO(kategorijaService.save(kategorija)), HttpStatus.OK);
     }
 
